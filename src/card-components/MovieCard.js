968c248283919e312395a31ad612664fff566a32
Yan C Lin <yonalin1@gmail.com>
2019-05-06 20:21:33 +0000

    Automatically backed up by Learn

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -40,4 +40,9 @@ export default class MovieCard extends Component {
   }
 }
 
-// Don't forget your default props!
+MovieCard.defaultProps = {
+  title: 'Unknown',
+  IMDBRating: null,
+  genres: ['No Genre(s) Found'],
+  poster: 'default',
+}
